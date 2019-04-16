ce6c760c0f8bf68417edea25942dc880a8c21f5a
sweetpotato <justsweetpotato@gmail.com>
2019-04-16 20:19:07 +0800

    添加网站图标

--- django_book/django_book/settings.py
+++ django_book/django_book/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!v#7d*l09)p!q)k#fyx_n$+9!x_^_&r&mgg43leja+2g+jqt)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'sweetpotato.xyz', 'www.sweetpotato.xyz']
 
